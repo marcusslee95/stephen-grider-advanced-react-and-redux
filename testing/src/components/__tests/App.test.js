cc7b1e5291fc0959b4babe430cebc9d746990bde
Marcus Lee <marcusslee95@berkeley.edu>
2021-03-15 12:07:58 -0700

    wrote enzyme version of 1st test

--- testing/src/components/__tests/App.test.js
+++ testing/src/components/__tests/App.test.js
@@ -1,27 +1,32 @@
 //Will write tests using enzyme -> using react testing library would be ideal because that's gaining steam while there are signs enzyme loosing steam (i.e. Airbnb created enzyme but now letting open source maintain it + they're using more and more of react testing library + enzyme doesnt have an adapter for react v17 available yet which is why i'm using an unofficial version) but that would mean I'd have to figure out react testing library way to do everything grider does which might take too much time (esp given tight window until interview)
 
 import React from 'react'
-import ReactDOM from 'react-dom'
 import App from '../App'
+import ComponentBox from '../CommentBox'
 //enzyme stuff
-import Enzyme from 'enzyme'
+import Enzyme, { shallow } from 'enzyme'
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
 Enzyme.configure({ adapter: new Adapter() });
 
 it('shows the CommentBox component', () => {
-    const div = document.createElement('div') //create a div element in JSDom
-    ReactDOM.render(<App/>, div) //put App component inside div we just created -> now that we have our App component on the JSDom in code below we can check to see if it is working liked we want it to 
-
-    console.log(div.innerHTML)
-    // console.log(div)
-
-    expect(div.innerHTML).toContain('Comment Box') //assumes knowledge about another component - that CommentBox component has 'Comment Box' inside it - tests like these are to be avoided
-
-    ReactDOM.unmountComponentAtNode(div) // deletes App component from div element -> for cleanup
+    const stuffInsideAppComponentThatWasRendered = shallow(<App/>) //renders everything inside the App component besides child components inside it (though it does create a bookmark / placeholder saying a child component does exist here) -> returns an object of the stuff we rendered
+    expect(stuffInsideAppComponentThatWasRendered.find(ComponentBox).length).toEqual(1)
 })
 
 
 
+//No testing library version of the 1st test I created 
+// it('shows the CommentBox component', () => {
+//     const div = document.createElement('div') //create a div element in JSDom
+//     ReactDOM.render(<App/>, div) //put App component inside div we just created -> now that we have our App component on the JSDom in code below we can check to see if it is working liked we want it to 
+
+//     console.log(div.innerHTML)
+//     // console.log(div)
+
+//     expect(div.innerHTML).toContain('Comment Box') //assumes knowledge about another component - that CommentBox component has 'Comment Box' inside it - tests like these are to be avoided
+
+//     ReactDOM.unmountComponentAtNode(div) // deletes App component from div element -> for cleanup
+// })
 
 
 //React testing library version of the 1st test I created 
