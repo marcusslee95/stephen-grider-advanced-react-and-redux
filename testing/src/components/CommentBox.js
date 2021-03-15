c647ef16f01c0c27947210c7af477b2629815619
Marcus Lee <marcusslee95@berkeley.edu>
2021-03-15 13:50:24 -0700

    fleshed out CommentBox component before going to test it

--- testing/src/components/CommentBox.js
+++ testing/src/components/CommentBox.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const CommentBox = () => {
-    return (
-        <div>
-            Comment Box
-        </div>
-    )
+class CommentBox extends Component {
+    state = {
+        comment: ''
+    }
+
+    handleChange = (event) => {//event will refer to the change event aka. user typed in something
+        this.setState({comment: event.target.value})
+    }
+
+    handleSubmit = (event) => {//event will refer to the submit event
+        event.preventDefault() //submit event by default causes page to reload -> we don't want that
+
+        //TODO - change state of commentList -> add this new comment to it
+
+        this.setState({comment: ''}) //after user submits a comment clear out the text area 
+
+    }
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <h4>Add a Comment</h4>
+                    <textarea onChange={this.handleChange} value={this.state.comment}/>
+                    <div><button>Submit Comment</button></div>
+                </form>
+            </div>
+        )
+    }
 }
 
 export default CommentBox
