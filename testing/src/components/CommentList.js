f812bbc3a3785913dc067676779d80ee380723bd
Marcus Lee <marcusslee95@berkeley.edu>
2021-03-16 16:05:38 -0700

    fleshed out commentList component so that we can test it next -> in particular had it generate list items out of the comments that it received from state

--- testing/src/components/CommentList.js
+++ testing/src/components/CommentList.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
-const CommentList = () => {
-    return (
-        <div>Commment List</div>
-    )
+class CommentList extends Component {
+
+
+    createListOfComments = () => {
+        return this.props.comments.map((comment, index) => {
+            return <li id={index}>{comment}</li>
+        })
+    }
+
+    render(){
+        return (
+            <ul>
+                {this.createListOfComments()}
+            </ul>
+        )
+    }
 }
 
-export default CommentList
+const mapStateToProps = (state) => { //takes the global state and returns only the part of the state this component compares about -> then passes those values as props to component
+    return {comments: state.comments}
+}
+export default connect(mapStateToProps)(CommentList)
--- testing/src/reducers/index.js
+++ testing/src/reducers/index.js
@@ -1,7 +1,8 @@
-import comments from './commentsReducer'
+import commentsReducer from './commentsReducer'
 import { combineReducers } from 'redux'
 
-const reducer = combineReducers({ comments }) //aka. the thing that takes in all actions and changes state accordingly
-//NOTE: initial state gets set here -> combineReducers will look at variable names you pass in and set them as the keys of the global state object -> it will then look at all the initial values inside the reducer functions and use those as the initial values -> i.e. if I had combineReducers( { reducer1, reducer2, reducer3 }) global state object would look like { reducer1: whateverInitialStateWasInReducer1, reducer2: whateverYougetthepoint, reducer3: youGetthePoint} -> here since it's just 1 reducer it'll be {comments: []} -> more info:  cmobined reducers section in https://redux.js.org/recipes/structuring-reducers/initializing-state
-
+const reducer = combineReducers({ 
+    comments: commentsReducer
+}) //aka. the thing that takes in all actions and changes state accordingly
+//NOTE: initial state gets set here -> it's weird even though you're creating the reducer you can think of it as also creating the initial state of the global state object so in this case global state object is {comments: [] } because we set initial state in commentsReducer to be [] and since it's in charge of comments property in the global state object it sets comments w/that value
 export default reducer
